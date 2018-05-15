9627ca988d603a95aaa17300c4d4ac243cc96dff
Siarhei Bucha <catonthescate@gmail.com>
2018-05-15 23:02:50 +0300

    first commit

---
+++ app/src/main/java/com/example/sergey/mytest/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.sergey.mytest
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : Activity(), View.OnClickListener{
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.button -> {Toast.makeText(this,"gggg",Toast.LENGTH_SHORT).show()
+            textView.setText("OK")}
+
+        }
+
+    }
+
+    lateinit var button : Button
+    lateinit var textView : TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        button = findViewById(R.id.button)
+        textView = findViewById(R.id.textView)
+
+        button.setOnClickListener(this)
+
+    }
+}
